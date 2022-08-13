74a061b296978b3ca091c0322e5f43015a244d03
devalpi <admin@alpi.dev>
2022-08-13 16:49:52 +0300

    feat: move src files of nextjs into src dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import '../../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
